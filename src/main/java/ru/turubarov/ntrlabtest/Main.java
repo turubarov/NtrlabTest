12f5034c8329167c680f0584cee6515e9e1b3cd4
Alex <turubarov88@mail.ru>
2018-05-07 16:38:43 +0700

    Refactoring code

--- src/main/java/ru/turubarov/ntrlabtest/Main.java
+++ src/main/java/ru/turubarov/ntrlabtest/Main.java
@@ -6,10 +6,12 @@ import org.jsoup.nodes.Document;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -20,31 +22,24 @@ public class Main {
             String str;
             StringBuilder htmlCode = new StringBuilder();
             while (in.readLine() != null) {
-                str = in.readLine().toString();
+                str = in.readLine();
                 htmlCode.append(str);
             }
             in.close();
             Document doc = Jsoup.parse(htmlCode.toString());
             String textOfWebPage = doc.body().text();
             Map<String, Integer> countWords = new LinkedHashMap<>();
-            for (int i = 0; i < args.length; i++) {
-                countWords.put(args[i], 0);
+            for (String arg : args) {
+                countWords.put(arg, 0);
             }
             for (String t : textOfWebPage.split(" ")) {
                 countWords.computeIfPresent(t, (k, v) -> v + 1);
             }
             List<Map.Entry<String, Integer>> list = new ArrayList<>(countWords.entrySet());
-            list.sort(Map.Entry.comparingByValue(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return o2 - o1;
-                }
-            }));
+            list.sort(Map.Entry.comparingByValue((o1, o2) -> o2 - o1));
             for (Map.Entry entry : list) {
                 System.out.println(entry.getKey()+ " " + entry.getValue());
             }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
