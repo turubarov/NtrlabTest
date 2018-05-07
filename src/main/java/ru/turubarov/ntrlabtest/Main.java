5571702aa442e373791b2b368da87a74a266b935
Alex <turubarov88@mail.ru>
2018-05-07 16:36:38 +0700

    Refactoring code

--- src/main/java/ru/turubarov/ntrlabtest/Main.java
+++ src/main/java/ru/turubarov/ntrlabtest/Main.java
@@ -26,22 +26,12 @@ public class Main {
             in.close();
             Document doc = Jsoup.parse(htmlCode.toString());
             String textOfWebPage = doc.body().text();
-            String[] wordsFrowText = textOfWebPage.split(" ");
-            for (int i = 0; i < wordsFrowText.length; i++) {
-                wordsFrowText[i] = wordsFrowText[i].toLowerCase();
-            }
             Map<String, Integer> countWords = new LinkedHashMap<>();
             for (int i = 0; i < args.length; i++) {
-                countWords.put(args[i].toLowerCase(), 0);
+                countWords.put(args[i], 0);
             }
-            for (int i = 0; i < wordsFrowText.length; i++) {
-                for (String searchWord : countWords.keySet()) {
-                    if (searchWord.equals(wordsFrowText[i])) {
-                        Integer count = countWords.get(searchWord);
-                        countWords.put(searchWord, ++count);
-                        continue;
-                    }
-                }
+            for (String t : textOfWebPage.split(" ")) {
+                countWords.computeIfPresent(t, (k, v) -> v + 1);
             }
             List<Map.Entry<String, Integer>> list = new ArrayList<>(countWords.entrySet());
             list.sort(Map.Entry.comparingByValue(new Comparator<Integer>() {
